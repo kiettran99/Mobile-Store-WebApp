eaa6ddd5517331631dca8ceef89c50427b33eaf0
Kiet Tran <17110165@student.hcmute.edu.vn>
2020-09-04 10:34:43 +0700

    Setup serval configures

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,39 @@
 const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
 
-module.exports = {
-  entry: './src/app.js',
-  output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'bundle.js',
-    publicPath: '/'
-  },
-  module: {
-    rules: [{
-      loader: 'babel-loader',
-      test: /\.js$/,
-      exclude: /node_modules/
-    }, {
-      test: /\.s?css$/,
-      use: ['style-loader', 'css-loader', 'sass-loader']
-    }]
-  },
-  devtool: 'cheap-module-eval-source-map',
-  devServer: {
-    contentBase: path.join(__dirname, 'public'),
-    disableHostCheck: true,
-    historyApiFallback: true
-  }
+module.exports = (env, argv) => {
+
+  const isProduction = env === 'production';
+
+  return {
+    entry: './src/app.js',
+    output: {
+      path: path.join(__dirname, 'public'),
+      filename: 'bundle.js',
+      publicPath: '/'
+    },
+    module: {
+      rules: [{
+        loader: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+      }, {
+        test: /\.s?css$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      }]
+    },
+    plugins: [
+      new TerserPlugin({
+        terserOptions: {
+          compress: argv['optimize-minimize'] // only if -p or --optimize-minimize were passed
+        }
+      })
+    ],
+    devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
+    devServer: {
+      contentBase: path.join(__dirname, 'public'),
+      disableHostCheck: true,
+      historyApiFallback: true
+    }
+  };
 };
