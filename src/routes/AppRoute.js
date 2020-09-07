3b420c99dcf3174ec578f80211984df8cf45bc45
Kiet Tran <17110165@student.hcmute.edu.vn>
2020-09-07 17:13:13 +0700

    Add lazing loading component in AppRoute.js

--- src/routes/AppRoute.js
+++ src/routes/AppRoute.js
@@ -1,14 +1,23 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, Suspense, lazy } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
 import Header from '../components/layout/Header';
-import Login from '../components/auth/Login';
+// import Login from '../components/auth/Login';
 import Alert from '../components/layout/Alert';
-import Register from '../components/auth/Register';
-import ProductList from '../components/products/ProductList';
-import AddProduct from '../components/products/product-forms/AddProduct';
-import ProductDetail from '../components/products/ProductDetail';
-import NotFoundPage from '../components/not-found-page/NotFoundPage';
+// import Register from '../components/auth/Register';
+// import ProductList from '../components/products/ProductList';
+// import AddProduct from '../components/products/product-forms/AddProduct';
+// import ProductDetail from '../components/products/ProductDetail';
+// import NotFoundPage from '../components/not-found-page/NotFoundPage';
 import PrivateRoute from '../components/routing/PrivateRoute';
+import Spinnet from '../components/layout/Spinnet';
+
+const Login = lazy(() => import('../components/auth/Login'));
+const Register = lazy(() => import('../components/auth/Register'));
+const ProductList = lazy(() => import('../components/products/ProductList'));
+const AddProduct = lazy(() => import('../components/products/product-forms/AddProduct'));
+const ProductDetail = lazy(() => import('../components/products/ProductDetail'));
+const NotFoundPage = lazy(() => import('../components/not-found-page/NotFoundPage'));
 
 const AppRoute = () => (
     <BrowserRouter>
@@ -16,14 +25,16 @@ const AppRoute = () => (
             <Header />
             <section className='container'>
                 <Alert />
-                <Switch>
-                    <Route exact path={["/", "/products"]} component={ProductList} />
-                    <Route exact path="/login" component={Login} />
-                    <Route exact path="/register" component={Register} />
-                    <Route exact path="/products/:id" component={ProductDetail} />
-                    <PrivateRoute exact path="/products/add" component={AddProduct} />
-                    <Route component={NotFoundPage} />
-                </Switch>
+                <Suspense fallback={<Spinnet />}>
+                    <Switch>
+                        <Route exact path={["/", "/products"]} component={ProductList} />
+                        <Route exact path="/login" component={Login} />
+                        <Route exact path="/register" component={Register} />
+                        <Route exact path="/products/:id" component={ProductDetail} />
+                        <PrivateRoute exact path="/products/add" component={AddProduct} />
+                        <Route component={NotFoundPage} />
+                    </Switch>
+                </Suspense>
             </section>
         </Fragment>
     </BrowserRouter>
